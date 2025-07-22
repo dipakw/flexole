b5e2ee3c2279893eb92ff379382136b11db99f52
Dipak Acharya <dev.dipak21@gmail.com>
2025-07-22 11:15:49 +0545

    Use refactored config

--- app/app.go
+++ app/app.go
@@ -1,42 +1,43 @@
 package app
 
 import (
+	"fmt"
 	"os"
 )
 
 func Run() {
 	if len(os.Args) < 2 {
-		showHelp("No command provided")
+		fmt.Println(cli_doc)
 		return
 	}
 
-	cmd, opts := cliArgs()
+	cmd := os.Args[1]
 
 	switch cmd {
 	case "server":
-		config, err := getServerConfig(opts)
+		config, err := getServerConfig()
 
 		if err != nil {
-			showHelp(err.Error())
-			return
+			fmt.Println(err)
+			os.Exit(1)
 		}
 
 		startServer(config)
 
 	case "client":
-		config, err := getClientConfig(opts)
+		config, err := getClientConfig()
 
 		if err != nil {
-			showHelp(err.Error())
-			return
+			fmt.Println(err)
+			os.Exit(1)
 		}
 
 		startClient(config)
 
 	case "help":
-		showHelp("")
+		fmt.Println(cli_doc)
 
 	default:
-		showHelp("Unknown command: " + cmd)
+		fmt.Println("Unknown command: " + cmd)
 	}
 }
